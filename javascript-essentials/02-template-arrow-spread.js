3a2388cb0ee576204436ecb6765b3233ea88d846
Fatin Israq Talha <israqq2120@gmail.com>
2025-12-01 13:07:09 +0600

    Add template strings, arrow functions, and spread operators example

---
+++ javascript-essentials/02-template-arrow-spread.js
@@ -0,0 +1,21 @@
+// Template String
+const lName = "Talha";
+const fName = "Fatin";
+
+console.log(fName + lName);
+console.log(`${fName} ${lName}`); // This is template string
+
+// Functions
+// function sum() {
+//     //
+// }
+
+// Arrow functions
+const sum = (a, b) => a + b;
+
+const result = sum(1, 2);
+console.log(result);
+
+// Spread Operators
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(...numbers);
