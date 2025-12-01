1ef6769a828589eca1df9a5280a422e8d668c0c7
Fatin Israq Talha <israqq2120@gmail.com>
2025-12-01 13:07:05 +0600

    Add JavaScript fundamentals example with variable declarations and array methods

---
+++ javascript-essentials/01-six-js-fundamentals.js
@@ -0,0 +1,32 @@
+// variable
+
+// / let and const
+
+let name = "Talha";
+name = "Fatin Israq"; // Can be changed
+
+const countryName = "BD"; // const can not be changed later
+
+// If else
+
+const num = 20;
+if (num >= 20 || num <= 30) {
+  console.log("YES");
+} else {
+  console.log("NO");
+}
+
+// Array Methods
+const friends = ["Hero", "Joker", "Batman"];
+
+friends.push("Superman"); // add element at the end
+console.log(friends);
+
+friends.pop(); // removes last element
+console.log(friends);
+
+friends.unshift("Superman"); // add element from first
+console.log(friends);
+
+friends.shift("Superman"); // removes first element
+console.log(friends);
